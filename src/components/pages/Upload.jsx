0ad3ebc913dae88511257f4a354cafa9c6838621
KashifKhn <kashifkhnx04@gmail.com>
2024-07-05 13:50:16 +0500

    feat: add login form and also fix upload file

--- src/components/pages/Upload.jsx
+++ src/components/pages/Upload.jsx
@@ -3,11 +3,15 @@
 import "./Upload.css";
 import { useState } from "react";
 import axios from "axios";
+import Spinner from "react-bootstrap/Spinner";
+import { useNavigate } from "react-router-dom";
+import Form from "react-bootstrap/Form";
 
 export const Upload = ({ setResponse }) => {
   const [file, setFile] = useState(null);
   const [error, setError] = useState(null);
   const [isLoading, setIsLoading] = useState(false);
+  const navigate = useNavigate();
 
   const submit = async () => {
     const data = new FormData();
@@ -21,6 +25,7 @@ export const Upload = ({ setResponse }) => {
         },
       });
       setResponse(res.data);
+      navigate("/result");
     } catch (err) {
       setError(err);
     } finally {
@@ -28,27 +33,70 @@ export const Upload = ({ setResponse }) => {
     }
   };
 
+  // const submit = async () => {
+  //   setIsLoading(true);
+  //   const fakePromise = new Promise((resolve, reject) => {
+  //     setTimeout(() => {
+  //       const fakeResponse = {
+  //         predictions: [
+  //           [
+  //             0.7649041414260864, 0.22852472960948944, 0.7203370928764343,
+  //             0.5890071392059326, 0.7507527470588684, 0.3222948908805847,
+  //             0.19163395464420319, 0.6751775145530701, 0.5685198903083801,
+  //             0.17052815854549408, 0.7105242013931274, 0.5096560120582581,
+  //             0.42698943614959717, 0.7888472676277161,
+  //           ],
+  //         ],
+  //       };
+  //       resolve(fakeResponse);
+  //       navigate("/result");
+  //     }, 3000); // 2 seconds delay
+  //   });
+  //   try {
+  //     const res = await fakePromise;
+  //     setResponse(res);
+  //   } catch (err) {
+  //     setError(err);
+  //   } finally {
+  //     setIsLoading(false);
+  //   }
+  // };
+  console.log(file);
+
   return (
     <div className="mainContainer">
       <div className="container">
-        <div className="flex justify-content-center align-items-center container1">
-          <input
+        <Form.Group
+          controlId="formFileLg"
+          className="mb-3">
+          <Form.Label>Upload file</Form.Label>
+          <Form.Control
             type="file"
-            id="file"
+            size="lg"
             disabled={isLoading}
             onChange={(e) => {
               setFile(e.target.files[0]);
             }}
           />
-        </div>
-        <div className="container2"></div>
+        </Form.Group>
       </div>
 
       <div className="abc">
         <div className="def"></div>
         <div className="efg">
-          <button onClick={submit} disabled={isLoading} className="btn2">
-            {isLoading ? "Loading..." : "Detect"}
+          <button
+            onClick={submit}
+            disabled={isLoading}
+            className="btn2">
+            {isLoading ? (
+              <Spinner
+                animation="border"
+                role="status">
+                <span className="visually-hidden">Loading...</span>
+              </Spinner>
+            ) : (
+              "Detect"
+            )}
           </button>
         </div>
       </div>
