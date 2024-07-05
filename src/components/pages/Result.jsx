6c7379b2368a295222975e82b7dc53a7a838f997
KashifKhn <kashifkhnx04@gmail.com>
2024-07-05 12:24:35 +0500

    feat: add bar charts and fix  ResultTable

--- src/components/pages/Result.jsx
+++ src/components/pages/Result.jsx
@@ -1,12 +1,13 @@
-import React from 'react'
-import { ResultTable } from './ResultTable'
-import './Result.css'
- export const Result = () => {
+/* eslint-disable no-unused-vars */
+/* eslint-disable react/prop-types */
+import React from "react";
+import { ResultTable } from "./ResultTable";
+import "./Result.css";
+export const Result = ({ response }) => {
   return (
     <div>
-       <h2 className='heading'>Probability of Injury</h2>
-      <ResultTable/>
+      <h2 className="heading">Probability of Injury</h2>
+      <ResultTable response={response} />
     </div>
-  )
-}
-
+  );
+};
---
+++ src/data/data.js
@@ -0,0 +1,58 @@
+export const data = [
+  {
+    id: 1,
+    organs: "bowel_healthy",
+  },
+  {
+    id: 2,
+    organs: "bowel_injury",
+  },
+  {
+    id: 3,
+    organs: "extravasation_healthy",
+  },
+  {
+    id: 4,
+    organs: "extravasation_injury",
+  },
+  {
+    id: 5,
+    organs: "kidney_healthy",
+  },
+  {
+    id: 6,
+    organs: "kidney_low",
+  },
+  {
+    id: 7,
+    organs: "kidney_high",
+  },
+  {
+    id: 8,
+    organs: "liver_healthy",
+  },
+  {
+    id: 9,
+    organs: "liver_low",
+  },
+  {
+    id: 10,
+    organs: "liver_high",
+  },
+  {
+    id: 11,
+    organs: "spleen_healthy",
+  },
+  {
+    id: 12,
+    organs: "spleen_low",
+  },
+  {
+    id: 13,
+    organs: "spleen_high",
+  },
+  {
+    id: 14,
+    organs: "any_injury",
+  },
+];
